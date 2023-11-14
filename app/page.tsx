a2de3b7d0ddd4dd6e571a67145b009ecda8174a9
amabelleS <amabelletr@gmail.com>
2023-11-14 18:03:37 +0200

    files structure: moved app/layuot.. to app.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='text-green-200 text-2xl flex flex-col items-center justify-center p-4'>
-      <h1 className='font-bold text-2xl flex items-center justify-center p-4'>Next.js Workshop 🤩</h1>
+      <h1 className='font-bold text-4xl capitalize border-b-2 border-green-200 flex items-center justify-center mb-6'>Next.js Workshop 🤩</h1>
       <div>
         <Link href="/posts" className='m-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded'>
           Posts
